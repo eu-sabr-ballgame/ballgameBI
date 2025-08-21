3c72e11a395da7e44a300d25508b95f16fe35369
Tony Pérez <tonper19@gmail.com>
2025-08-21 21:08:08 +0100

    schedule  task keeps failing

--- bi/scripts/update_repo.ps1
+++ bi/scripts/update_repo.ps1
@@ -137,12 +137,8 @@ if ($failed) { exit 3 } else { exit 0 }
 # (Recommended) Run with highest privileges
 # If you want it headless: Run whether user is logged on or not
 
-# schtasks /create /tn "UpdateBallgameRepoDaily" ^
-#  /tr "cmd /c \"powershell.exe -ExecutionPolicy Bypass -File C:\baseball\ballgameBI\update_repo.ps1\"" ^
-#  /sc daily /st 20:00
-# schtasks /create /tn "UpdateBallgameRepoOnLogon" ^
-#  /tr "cmd /c \"powershell.exe -ExecutionPolicy Bypass -File C:\baseball\ballgameBI\update_repo.ps1\"" ^
-#  /sc onlogon
+# schtasks /create /tn "UpdateBallgameRepoDaily" ^ /tr "cmd /c \"powershell.exe -ExecutionPolicy Bypass -File C:\baseball\ballgameBI\update_repo.ps1\"" ^  /sc daily /st 20:00
+# schtasks /create /tn "UpdateBallgameRepoOnLogon" ^ /tr "cmd /c \"powershell.exe -ExecutionPolicy Bypass -File C:\baseball\ballgameBI\update_repo.ps1\"" ^ /sc onlogon
 
 # Viewing the Event Log entries
 # Open Event Viewer → Windows Logs → Application
